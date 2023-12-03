57d0a7c26b6d44116f23d796a583cd79a1091343
[Younes HAMMADI] <younes.hammadi.etu@gmail.com>
2023-12-03 18:31:29 +0100

    adding the model file with the descriminator

---
+++ trainer.py
@@ -0,0 +1,7 @@
+import numpy as np
+import torch.nn as nnn
+import torch.optim as optim 
+
+from decoder import FaderNetworksDecoder
+from encoder import FaderNetworkDecoder
+from discriminator import FaderNetworksDiscriminator
