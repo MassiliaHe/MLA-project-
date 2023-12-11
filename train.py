d82704064c6c853c5d82fa4e233eb2b78c0d41b3
Ouali_Sofiane <ouali.199602@gmail.com>
2023-12-12 00:28:09 +0100

    update train_laoder and val_laoder

--- train.py
+++ train.py
@@ -8,27 +8,31 @@
 import torch
 from torch.utils.data import DataLoader
 
-from dataset.dataloader import CelebADataset
+from dataset.dataloader import CelebADataset, split_data
 from FaderNetwork.autoencoder import AutoEncoder
 from FaderNetwork.classificateur import Classifier
 from FaderNetwork.discriminator import Discriminator
 from utils.training import autoencoder_step, classifier_step, discriminator_step
 
 
-def train():
+def train(base_dir,annotations_file,list_eval_partition,Attr):
     # Specify the path to your CelebA dataset
-    celeba_root = '/path/to/celeba/dataset'
-
+    # base_dir = img_align_celeba'#chemin vers les images 
+    # annotations_file = list_attr_celeba.csv 
+    # Use split_data to get the image IDs
+    #Attr attribue qu'on souhaite extraire
+    dataset_ids = split_data(list_eval_partition)
+    # Créer des instances de CelebADataset pour chaque ensemble
     # Create an instance of the CelebADataset with specified transformations
-    # TODO Instantiate with specific parameters
-    celeba_dataset = CelebADataset(root_dir=celeba_root, image_size=(64, 64), normalize=True)
-
+    celeba_dataset_train =  CelebADataset(base_dir, annotations_file,Attr, dataset_ids['train'])
+    celeba_dataset_val =  CelebADataset(base_dir, annotations_file,Attr, dataset_ids['validation'])
     # Specify batch size and whether to shuffle the data
     batch_size = 64
-    shuffle = True
 
     # Create DataLoader TODO Instantiate with specific parameters
-    celeba_dataloader = DataLoader(dataset=celeba_dataset, batch_size=batch_size, shuffle=shuffle)
+    celeba_dataloader_train = DataLoader(dataset=celeba_dataset_train, batch_size=batch_size, shuffle=True)
+    celeba_dataloader_val= DataLoader(dataset=celeba_dataset_val, batch_size=batch_size, shuffle=False)
+
 
     # Create instances of your models (Encoder, Decoder, Classifier, Discriminator)
     # TODO Instantiate with specific parameters
