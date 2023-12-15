c42b23d62fd6a8e5383f3ef285c07e7a4812d289
MassiliaHe <massilia.helifi@gmail.com>
2023-12-15 19:07:58 +0100

    code of steps foenctions discriminator,  AE and total stepfonctions

--- utils/training.py
+++ utils/training.py
@@ -1,43 +1,23 @@
 import torch
 import torch.nn as nn
 
-
-
 def classifier_step(classifier, images, attributes, classifier_optimizer):
-
     """
-    Perform a single training step for the classifier.
-    
-    Arguments:
-    - classifier: the neural network model.
-    - images: a batch of input images.
-    - attributes: the ground truth binary attributes for the images.
-    - classifier_optimizer: optimizer for the classifier model.
-    
-    Returns:
-    - loss.item(): The binary cross-entropy loss for the current batch.
+    Train the classifier.
     """
+    classifier.train()
+    # TODO
+    ## batch / classify
+    preds = classifier(images)
 
-    #set the classifier to training mode
-    classifier.train() 
-    #forward pass: compute the predicted outputs. 
-    preds = classifier(images)  
-    
-    #create a loss function for binary classification
-    loss_function = nn.BCEWithLogitsLoss() 
-    #compute the loss 
-    loss = loss_function(preds, attributes)  
-    
-    #zero the gradients of the classifier parameters
-    classifier_optimizer.zero_grad() 
-    #perform backpropagation to calculate gradients
-    loss.backward()  
-    #update the classifier parameters
-    classifier_optimizer.step()  
+    ## loss / optimize
 
-    #return the loss as a Python float
-    return loss.item()  
+    loss = 0 
+    classifier_optimizer.zero_grad()
+    loss.backward()
+    classifier_optimizer.step()
 
+    return loss
 
 def autoencoder_step(autoencoder, discriminator, images, attributes, autoencoder_optimizer, criterion):
     """
@@ -56,23 +36,31 @@ def autoencoder_step(autoencoder, discriminator, images, attributes, autoencoder
 
     return loss.item()
 
-def discriminator_step(discriminator, autoencoder, images, attributes, discriminator_optimizer):
+def discriminator_step(discriminator, autoencoder, images, attributes, discriminator_optimizer, criterion):
     """
-    Train the classifier.
+    Train the discriminator.
     """
-
-    criterion = nn.BCELoss()
-
     discriminator.train()
-    # TODO
-    ## batch / classify
-    preds = discriminator(images)
-
-    ## loss / optimize
-    loss = criterion(preds, attributes.float())
+    # Generate fake attributes using the autoencoder
+    with torch.no_grad():
+        encoded_imgs = autoencoder.encode(images)
+        fake_attributes = discriminator(encoded_imgs)
+    # Calculate loss
+    real_loss = criterion(discriminator(images), attributes.float())
+    fake_loss = criterion(fake_attributes, torch.zeros_like(fake_attributes))
+    loss = (real_loss + fake_loss) / 2
+    # Backpropagation and optimization
     discriminator_optimizer.zero_grad()
     loss.backward()
-    discriminator_optimizer.step() 
+    discriminator_optimizer.step()
 
-    return loss
+    return loss.item()
+
+def step(autoencoder,classifier_optimizer, discriminator, images, attributes, autoencoder_optimizer,classifier, criterion, discriminator_optimizer):
+    clf_loss = classifier_step(classifier, images, attributes, classifier_optimizer, criterion)
+    # Train autoencoder
+    ae_loss = autoencoder_step(autoencoder, discriminator, images, attributes, autoencoder_optimizer, criterion)
+    # Train discriminator
+    dis_loss = discriminator_step(discriminator, autoencoder, images, attributes, discriminator_optimizer, criterion)
+    return clf_loss, ae_loss, dis_loss
 
