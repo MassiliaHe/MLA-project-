f4e29e0e024aa186227b52a376e93bb197f18fcf
MassiliaHe <massilia.helifi@gmail.com>
2023-12-16 16:30:15 +0100

    debug a discriminator

--- FaderNetwork/discriminator.py
+++ FaderNetwork/discriminator.py
@@ -4,17 +4,21 @@ import torch.nn as nn
 class Discriminator(nn.Module):
     def __init__(self, n_attributes):
         super(Discriminator, self).__init__()
-        # Following the paper, we assume the latent code has a size of 512
-        self.model = nn.Sequential(
-            nn.Linear(512, 1024),
+        self.n_attributes = n_attributes
+        # Assuming the flattened latent representation has a size of 2048 (e.g., 512 * 2 * 2)
+        self.discriminator = nn.Sequential(
+            nn.Linear(2048, 1024),  # Adjust the input features to match the flattened size.
             nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.3),
             nn.Linear(1024, 1024),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.3),
-            nn.Linear(1024, n_attributes),
+            nn.Linear(1024, self.n_attributes),
             nn.Sigmoid()
         )
 
     def forward(self, z):
-        return self.model(z)
+        # Flatten the latent representation if it's not already flat.
+        if z.dim() > 2:
+            z = z.view(z.size(0), -1)
+        return self.discriminator(z)
