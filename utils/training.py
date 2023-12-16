d5698e9e0bd699551cda1a11ce2e691c6054afc3
MassiliaHe <massilia.helifi@gmail.com>
2023-12-16 15:23:11 +0100

    change a step fonction for AE

--- utils/training.py
+++ utils/training.py
@@ -28,22 +28,54 @@ def classifier_step(classifier, images, attributes, classifier_optimizer):
     return loss.item()  
 
 
-def autoencoder_step(autoencoder, discriminator, images, attributes, autoencoder_optimizer, criterion):
+import torch
+
+def autoencoder_step(autoencoder, discriminator, images, attributes, autoencoder_optimizer, discriminator_optimizer, lambda_val, criterion, step_count):
     """
-    Train the autoencoder.
+    Perform a training step for both the autoencoder and the discriminator.
+
+    :param autoencoder: The AutoEncoder model.
+    :param discriminator: The Discriminator model.
+    :param images: The batch of images.
+    :param attributes: The batch of attributes associated with the images.
+    :param autoencoder_optimizer: The optimizer for the autoencoder.
+    :param discriminator_optimizer: The optimizer for the discriminator.
+    :param lambda_val: The weight for the adversarial loss.
+    :param criterion: The loss function for reconstruction (e.g., nn.MSELoss()).
+    :param step_count: The current step count for adjusting lambda_val.
+    :return: The reconstruction loss and adversarial loss.
     """
     autoencoder.train()
-    # Encode and decode images
-    encoded_imgs = autoencoder.encode(images)
-    decoded_imgs = autoencoder.decode(encoded_imgs, attributes)
-    # Calculate loss
-    loss = criterion(decoded_imgs, images)  # Assuming you have a reconstruction loss
-    # Backpropagation and optimization
+    discriminator.train()
+
+    # Forward pass through the autoencoder.
+    encoded_imgs, decoded_imgs = autoencoder(images, attributes)
+
+    # Compute the reconstruction loss.
+    reconstruction_loss = criterion(decoded_imgs, images)
+
+    # Forward pass through the discriminator.
+    attributes_pred = discriminator(encoded_imgs.detach())
+
+    # Compute the adversarial loss.
+    adversarial_loss = -torch.mean(torch.log(attributes_pred + 1e-8) * attributes + torch.log(1 - attributes_pred + 1e-8) * (1 - attributes))
+
+    # Update the discriminator.
+    discriminator_optimizer.zero_grad()
+    adversarial_loss.backward(retain_graph=True)  # Retain graph for further autoencoder update.
+    discriminator_optimizer.step()
+
+    # Compute the total loss for the autoencoder.
+    total_loss = reconstruction_loss + lambda_val(step_count) * adversarial_loss
+
+    # Update the autoencoder.
     autoencoder_optimizer.zero_grad()
-    loss.backward()
+    total_loss.backward()
     autoencoder_optimizer.step()
 
-    return loss.item()
+    return reconstruction_loss.item(), adversarial_loss.item()
+
+
 
 
 def discriminator_step(discriminator, autoencoder, images, attributes, discriminator_optimizer, criterion):
