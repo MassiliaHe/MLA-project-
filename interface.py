ca138d3d67305ab3003688e35ec275f860a41377
MassiliaHe <massilia.helifi@gmail.com>
2023-12-20 00:48:28 +0100

    Update the interface graphic

--- interface.py
+++ interface.py
@@ -2,13 +2,13 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, QHBoxLayout, QFileDialog
 from PyQt5.QtGui import QPixmap, QFont, QImage
 from PyQt5.QtCore import Qt
-import torch
 
-from dataset.dataloader import get_dataloaders
-import numpy as np
 import torch
-from torch.autograd import Variable
-from torchvision.utils import make_grid
+import numpy as np
+from PIL import Image
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
 
 class ImageProcessingApp(QWidget):
     def __init__(self):
@@ -55,33 +55,38 @@ class ImageProcessingApp(QWidget):
         self.load_button.clicked.connect(self.process_image)
         self.load_button_load.clicked.connect(self.set_data_path)
 
+        self.data_path = None
+
         self.setGeometry(100, 100, 500, 400)
         self.setWindowTitle('Fader Networks App')
         self.show()
 
     def set_data_path(self):
-        # Add fonction to ask for the path data
+        # Fonction pour demander le chemin d'un fichier
         options = QFileDialog.Options()
         # options = QFileDialog.DontUseNativeDialog
-        directory = QFileDialog.getExistingDirectory(self, "Sélectionner le dossier de données", options=options)
+        file_name, _ = QFileDialog.getOpenFileName(
+            self, "Sélectionner un fichier", "", "Tous les fichiers (*);;Images Files (*.png;*.jpg;*.jpeg)", options=options)
 
-        if directory:
-            print("Chemin sélectionné:", directory)
-            self.data_path = directory
+        if file_name:
+            print("Fichier sélectionné :", file_name)
+            self.data_path = file_name
 
     def process_image(self):
-        
-        input_image, output_image = UI_interpolation(self.data_path)
 
-        pixmap1 = self.tensor_to_pixmap(input_image[0], True)
-        pixmap2 = self.tensor_to_pixmap(output_image[0], False)
-        input_pixmap = QPixmap(pixmap1)
-        output_pixmap = QPixmap(pixmap2)
+        if self.data_path is not None:
 
-        # Afficher les images dans les labels correspondants
-        self.image_label_left.setPixmap(input_pixmap.scaledToWidth(200, Qt.SmoothTransformation))
-        self.image_label_right.setPixmap(output_pixmap.scaledToWidth(200, Qt.SmoothTransformation))
-    
+            input_image, output_image = predict(self.data_path)
+
+            pixmap1 = self.tensor_to_pixmap(input_image[0], True)
+            pixmap2 = self.tensor_to_pixmap(output_image[0], False)
+            input_pixmap = QPixmap(pixmap1)
+            output_pixmap = QPixmap(pixmap2)
+
+            # Afficher les images dans les labels correspondants
+            self.image_label_left.setPixmap(input_pixmap.scaledToWidth(200, Qt.SmoothTransformation))
+            self.image_label_right.setPixmap(output_pixmap.scaledToWidth(200, Qt.SmoothTransformation))
+
     def tensor_to_pixmap(self, tensor_image, np):
         # Convert the tensor image to a NumPy array
         if not np:
@@ -98,79 +103,37 @@ class ImageProcessingApp(QWidget):
         return pixmap
 
 
-def UI_interpolation(input_data):
-    # create logger / load trained model
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    ae = torch.load('models/best_autoencoder.pt').to(device)
-    
-    # restore main parameters
-    debug = True
-    batch_size = 32
-    v_flip = False
-    h_flip = False
-    img_sz = 256
-    attr = ['Young']
-    n_attr = 2
-    if not (len(attr) == 1 and n_attr == 2):
-        raise Exception("The model must use a single boolean attribute only.")
-    
-    # load dataset
-    
-    _, _, test_data = get_dataloaders(
-        input_data, name_attr=attr[0], batch_size=batch_size)
-    
-    
-    def get_interpolations(ae, images, attributes):
-        """
-        Reconstruct images / create interpolations
-        """
-        assert len(images) == len(attributes)
-        enc_outputs = ae.encode(images)
-    
-        # interpolation values
-        alphas = np.linspace(1 - 1, 1, 1)
-        alphas = [torch.FloatTensor([1 - alpha, alpha]).to(device) for alpha in alphas]
-    
-        # original image / reconstructed image / interpolations
-        outputs = []
-        outputs.append(images)
-        outputs.append(ae.decode(enc_outputs, attributes))
-        for alpha in alphas:
-            alpha = Variable(alpha.unsqueeze(0).expand((len(images), 2)))
-            outputs.append(ae.decode(enc_outputs, alpha))
-    
-        # return stacked images
-        return torch.cat([x.unsqueeze(1) for x in outputs], 1).data.cpu()
-    
-    
-    interpolations = []
-    
-    for k in range(0, 1, 100):
-        images, attributes = next(iter(test_data))
-        images, attributes = images.to(device), attributes.to(device)
-        interpolations.append(get_interpolations(ae, images[:1], attributes[:1]))
-    
-    interpolations = torch.cat(interpolations, 0)
-    assert interpolations.size() == (1, 2 + 1,
-                                    3, img_sz, img_sz)
-    
-    
-    def get_grid(images, row_wise, plot_size=5):
-        """
-        Create a grid with all images.
-        """
-        n_images, n_columns, img_fm, img_sz, _ = images.size()
-        if not row_wise:
-            images = images.transpose(0, 1).contiguous()
-        images = images.view(n_images * n_columns, img_fm, img_sz, img_sz)
-        images.add_(1).div_(2.0)
-        return make_grid(images, nrow=(n_columns if row_wise else n_images))
-    
-    
-    # generate the grid / save it to a PNG file
-    grid = get_grid(interpolations, True, 5)
-    normalized_image = (grid.cpu().numpy().transpose((1, 2, 0)) - grid.cpu().numpy().min()) / (grid.cpu().numpy().max() - grid.cpu().numpy().min())
-    return images, normalized_image
+def predict(img_path='data/img/1.jpeg', dec_path='models/_dec.pt', enc_path='models/_enc.pt', n_interpolations=10, max_attr=2.0, min_attr=2.0, save_fig_path='fig/fig.jpg', use_gpu=False, IMAGE_SIZE=256):
+
+    # Load Model
+    enc = torch.load(enc_path, map_location='cuda' if use_gpu else 'cpu')
+    dec = torch.load(dec_path, map_location='cuda' if use_gpu else 'cpu')
+
+    # Interpolate Attributes
+    alphas = np.linspace(1 - min_attr, max_attr, n_interpolations)
+    alphas = [[1 - alpha, alpha] for alpha in alphas]
+
+    with Image.open(img_path) as img:
+        img = img.crop((0, 0, 178, 178))
+        img = img.resize((IMAGE_SIZE, IMAGE_SIZE))
+        img = np.array(img, dtype=np.float32) / 127.5-1.0
+        img = transforms.ToTensor()(img).unsqueeze(0)
+
+    # Run test
+    test_img = img.float()
+    test_attr = torch.FloatTensor(alphas[1])
+    reconstruct_img = dec(enc(test_img), test_attr)
+
+    org_image = np.transpose((1+test_img.squeeze(0).cpu().data.numpy())/2, (1, 2, 0))
+    rec_image = np.transpose((1+reconstruct_img.squeeze(0).cpu().data.numpy())/2, (1, 2, 0))
+    if save_fig_path:
+        plt.imsave(save_fig_path, rec_image)
+        print('Result saved to', save_fig_path)
+        plt.close()
+
+    # Show result
+    return org_image, rec_image
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
